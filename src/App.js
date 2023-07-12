d21d27d4f8b73a8d17ea2d57c2b1ba5b54bb5491
4thdraw <4thdraw2@gmail.com>
2023-07-12 15:18:50 +0900

    리액트폰트설치및 상단위치감지하여 헤더숨기기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import ScrollSpyHeader from './layout/Hd';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ScrollSpyHeader />
+      <div className="section">
+        Section 1
+      </div>
+      <div name="section2" className="section">
+        Section 2
+      </div>
+      <div name="section3" className="section">
+        Section 3
+      </div>
     </div>
   );
 }
